2759aaf093e57437fa2508473a470a612771151f
I'mDuvalinBS <jg0455748@gmail.com>
2026-07-18 07:44:30 -0600

    Update exploration command cooldown and rewards

--- commands/explorar.js
+++ commands/explorar.js
@@ -2,13 +2,13 @@ import { workCommand } from "../core.js";
 
 export default {
   names: [".explorar", ".investigar"],
-  desc: "Exploración, ganancia media-alta (cada 4 horas)",
+  desc: "Exploración, ganancia media-alta (cada 30 minutos)",
   category: "Trabajos",
   handler: workCommand({
     key: "explorar",
-    cooldownMs: 4 * 60 * 60 * 1000,
-    minReward: 180,
-    maxReward: 350,
+    cooldownMs: 30 * 60 * 1000,
+    minReward: 500,
+    maxReward: 1200,
     frases: {
       titulo: "¡EXPLORACIÓN COMPLETADA!",
       exito: [
