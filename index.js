c0d10bd0a78b65680d9d882320425255da5e6a8c
I'mDuvalinBS <jg0455748@gmail.com>
2026-07-18 04:50:48 -0600

    Import handleEconomyCommand from shares.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import {
 import pino from "pino";
 import http from "http";
 import { handleEconomyCommand } from "./economia.js";
+import { handleEconomyCommand } from "./shares.js";
+
  
 const PHONE_NUMBER = "529616050619";
 
