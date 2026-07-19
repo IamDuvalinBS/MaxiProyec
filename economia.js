6235ad52a5b69747bdd6c3a2945e89cb75822239
I'mDuvalinBS <jg0455748@gmail.com>
2026-07-19 06:58:35 -0600

    Add perfil directory to carpetas array

--- economia.js
+++ economia.js
@@ -6,7 +6,8 @@ import { connectDB, commandRegistry, checkTriviaAnswer } from "./core.js";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const carpetas = [
   { dir: path.join(__dirname, "commands"), prefix: "./commands/" },
-  { dir: path.join(__dirname, "reacciones"), prefix: "./reacciones/" }
+  { dir: path.join(__dirname, "reacciones"), prefix: "./reacciones/" },
+  { dir: path.join(__dirname, "perfil"), prefix: "./perfil/" }
 ];
 
 const commandMap = new Map(); // cada nombre/alias -> handler
@@ -64,4 +65,4 @@ export async function handleEconomyCommand(sock, from, sender, text, msg) {
 }
 
 export { checkTriviaAnswer };
-  
+      
