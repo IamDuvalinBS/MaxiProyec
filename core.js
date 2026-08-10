4e22c04311e23acf313f2a2af1494ff30c70c051
I'mDuvalinBS <jg0455748@gmail.com>
2026-08-10 17:34:32 -0600

    Add additional exports to core.js
    Added exports for sticker.js, descargas-core.js, redes-core.js, youtube-core.js, and spotify-core.js.

--- core.js
+++ core.js
@@ -1,6 +1,6 @@
 // Este archivo NO tiene logica propia - solo reune todo lo de los archivos
-// chicos (db.js, ui.js, profile.js, trivia.js, reactions.js, work.js) para
-// que los comandos puedan seguir haciendo "import ... from ../core.js" como
+// chicos (db.js, ui.js, profile.js, trivia.js, reactions.js, work.js, sticker.js)
+// para que los comandos puedan seguir haciendo "import ... from ../core.js" como
 // siempre, sin tener que cambiar nada en cada archivo de comando.
 export * from "./db.js";
 export * from "./ui.js";
@@ -8,3 +8,8 @@ export * from "./profile.js";
 export * from "./trivia.js";
 export * from "./reactions.js";
 export * from "./work.js";
+export * from "./sticker.js";
+export * from "./descargas-core.js";
+export * from "./redes-core.js";
+export * from "./youtube-core.js";
+export * from "./spotify-core.js";
