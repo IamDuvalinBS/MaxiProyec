19bb8c58683d2dc0a2f59ff592e64accbb169157
I'mDuvalinBS <jg0455748@gmail.com>
2026-08-10 18:03:34 -0600

    Uncomment export for adminwelcome.js

--- core.js
+++ core.js
@@ -2,12 +2,6 @@
 // chicos (db.js, ui.js, profile.js, trivia.js, reactions.js, work.js, sticker.js)
 // para que los comandos puedan seguir haciendo "import ... from ../core.js" como
 // siempre, sin tener que cambiar nada en cada archivo de comando.
-export * from "./db.js";
-export * from "./ui.js";
-export * from "./profile.js";
-export * from "./trivia.js";
-export * from "./reactions.js";
-export * from "./work.js";
 export * from "./sticker.js";
 export * from "./descargas-core.js";
 export * from "./redes-core.js";
