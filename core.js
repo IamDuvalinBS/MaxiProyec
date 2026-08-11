6f0db1493298519d11e14191d50dc0d46145f59d
I'mDuvalinBS <jg0455748@gmail.com>
2026-08-10 18:30:37 -0600

    Refactor exports to use motores directory
    Updated export paths to use new directory structure.

--- core.js
+++ core.js
@@ -2,11 +2,9 @@
 // chicos (db.js, ui.js, profile.js, trivia.js, reactions.js, work.js, sticker.js)
 // para que los comandos puedan seguir haciendo "import ... from ../core.js" como
 // siempre, sin tener que cambiar nada en cada archivo de comando.
-export * from "./sticker.js";
-export * from "./descargas-core.js";
-export * from "./redes-core.js";
-export * from "./youtube-core.js";
-export * from "./spotify-core.js";
+export * from "./motores/sticker.js";
+export * from "./motores/redes.js";
+export * from "./motores/sp.js";
 export * from "./motores/db.js";
 export * from "./motores/ui.js";
 export * from "./motores/profile.js";
